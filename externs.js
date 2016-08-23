12deb4fe2b1766641a169400064a67ab21433f69
Olibaron <olivier@airwave.io>
2016-08-23 17:08:53 +0200

    first commit

---
+++ externs.js
@@ -0,0 +1,15 @@
+/**
+ * @constructor
+ *
+ * @param {string} family
+ * @param {Object} descriptors
+ */
+var FontFacePreloader = function (family, descriptors) {};
+
+/**
+ * @param {string=} opt_text
+ * @param {number=} opt_timeout
+ *
+ * @return {Promise.<FontFacePreloader>}
+ */
+FontFacePreloader.prototype.load = function (opt_text, opt_timeout) {};
---
+++ vendor/google/no-deps.js
@@ -0,0 +1,1 @@
+goog.global.CLOSURE_NO_DEPS = true;
